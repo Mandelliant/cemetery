facce095380d12d3895aaddd66906a94f659b1fc
leadcyber <travashley826@gmail.com>
2024-10-31 13:04:57 -0500

    addressed all issues

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,39 +3,65 @@ const originalHeight = 1076;
 
 const modalContentArray = [
   {
-    'header-title': 'Here lies a staking rewards contract.',
-    'header-description': 'It worked hard, distributed faithfully… but only for a little while.',
-    'content': "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Limited lifecycle</P>",
-    'main-content': '<p>This staking rewards contract started with a mission—to incentivize users, reward commitment, and build liquidity. But like so many before it, it was bound by the limits of a predetermined staking period. Once that period ended, the contract’s purpose was fulfilled, and it was left to expire—its potential halted, its participants needing to migrate or redeploy elsewhere.</p><p>This is the fate of short-lived staking contracts: they burn bright, but not for long. Their lifespans are predetermined, leaving users scrambling to redeploy or move liquidity once the contract reaches the end of its life.</p>',
+    "header-title": "Here lies a decentralized lending protocol",
+    "header-description":
+      "Loans were given, collateral secured... until cross-chain complexity stopped it in its tracks",
+    content:
+      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Bound by atomic operations</P>",
+    "main-content":
+      "<p>This lending platform faithfully executed its duties, offering loans and securing collateral on-chain with precision. Liquidations were fast, and interest rates competitive—for as long as everything happened within a single ecosystem. However, as users sought multi-chain lending capabilities, cracks began to show. It struggled to keep pace in a world where users wanted to borrow from one chain while collateral resided on another.</p><p>If only it had been built for asynchronous execution! Asynchronous liquidation triggers could monitor loans across chains, initiating liquidations even if collateral and debt resided on separate platforms. Liquidity aggregation could have drawn from multiple chains in real-time, optimizing interest rates and lowering loan-to-value risks without interruptions.</p><p>Orchestration would have brought this platform longer life.</p>",
+    "redirect-url":
+      "https://docs.agoric.com/guides/orchestration/how-orch-works.html",
   },
   {
-    'header-title': 'Here lies an auction contract.',
-    'header-description': 'The bids were high, the competition fierce... until it wasn’t.',
-    'content': "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Outbid by complexity</P>",
-    'main-content': "<p>This auction contract did what it was designed to do—host an auction, gather bids, and transfer assets to the winner when the time expired. However, once the auction ended, so did the contract's purpose. It was limited to a single auction cycle, forced to terminate and redeploy each time a new auction began. For developers, this meant added complexity and overhead. For participants, it meant missed opportunities and the inconvenience of waiting for new deployments.</p><p>This is the fate of short-lived auction contracts: they are built for one-off events. Every new auction requires fresh deployment, creating a fragmented user experience and adding unnecessary friction to what could otherwise be a seamless, continuous marketplace.</p>",
+    "header-title": "Here lies a single-chain yield aggregator",
+    "header-description":
+      "The returns were optimized, the profits were promising... but only within one chain",
+    content:
+      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Single-chain constriction</P>",
+    "main-content":
+      "<p>This yield aggregator set out to provide users with the best returns by shifting funds between high-yield opportunities—as long as everything remained on a single chain. It worked well within its own ecosystem, but the moment users sought higher yields across multiple blockchains, the aggregator faltered. It couldn’t manage rebalancing strategies across networks.</p><p>It could have asynchronously rebalanced vaults between multiple chains. Instead of relying on instant atomic operations, the aggregator could have shifted to an adaptive, asynchronous strategy that maximized returns smoothly across ecosystems. Unfortunately, limited to a single chain and real-time constraints, this yield aggregator missed its chance to thrive in a multi-chain world. </p> <p>With asynchronous contracts, this platform’s ending may have been different.</p>",
+    "redirect-url":
+      "https://docs.agoric.com/guides/orchestration/orchestration-basics/contract.html",
   },
   {
-    'header-title': 'Here lies a subscriptions contract.',
-    'header-description': 'It collected recurring fees… until it couldn’t keep up.',
-    'content': "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Lack of flexibility</P>",
-    'main-content': "<p>This auction contract did what it was designed to do—host an auction, gather bids, and transfer assets to the winner when the time expired. However, once the auction ended, so did the contract's purpose. It was limited to a single auction cycle, forced to terminate and redeploy each time a new auction began. For developers, this meant added complexity and overhead. For participants, it meant missed opportunities and the inconvenience of waiting for new deployments.</p><p>This is the fate of short-lived auction contracts: they are built for one-off events. Every new auction requires fresh deployment, creating a fragmented user experience and adding unnecessary friction to what could otherwise be a seamless, continuous marketplace.</p>",
+    "header-title": "Here lies a subscription contract",
+    "header-description":
+      "The fees were steady, the renewals consistent... until they weren’t.",
+    content:
+      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Buried under user intents</P>",
+    "main-content":
+      "<p>This subscription contract began with the goal of providing automated, recurring payments to ensure uninterrupted services for users. It operated well initially—until the complexities of modern blockchain environments overwhelmed it. Bound to a single chain, the contract couldn’t keep up as users demanded multi-chain payment options.</p><p>Every payment required the user’s active approval, which introduced friction, and lapses became common as users failed to keep up with approvals. As the contract tried to manage every interaction atomically, it was buried by the weight of complexity and manual intervention. Alas, this contract was constrained by outdated execution models, unable to meet the demands of a multi-chain world.</p><p>Orchestration accounts are an async secret weapon.</p>",
+    "redirect-url":
+      "https://docs.agoric.com/guides/orchestration/key-concepts.html#orchestration-account",
   },
   {
-    'header-title': 'Here lies an auction contract.',
-    'header-description': 'The bids were high, the competition fierce... until it wasn’t.',
-    'content': "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Outbid by complexity</P>",
-    'main-content': "<p>This auction contract did what it was designed to do—host an auction, gather bids, and transfer assets to the winner when the time expired. However, once the auction ended, so did the contract's purpose. It was limited to a single auction cycle, forced to terminate and redeploy each time a new auction began. For developers, this meant added complexity and overhead. For participants, it meant missed opportunities and the inconvenience of waiting for new deployments.</p><p>This is the fate of short-lived auction contracts: they are built for one-off events. Every new auction requires fresh deployment, creating a fragmented user experience and adding unnecessary friction to what could otherwise be a seamless, continuous marketplace.</p>",
+    "header-title": "Here lies an NFT auction contract",
+    "header-description":
+      "The bids were high, the competition fierce... until it wasn’t.",
+    content:
+      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Block-constrained execution</P>",
+    "main-content":
+      "<p>This NFT auction contract began with the promise of decentralized ownership and open competition. Yet, the excitement quickly waned as participants encountered the limitations of their single-chain environment. When people routinely have NFTs and liquidity spanning multiple chains, this contract’s limited scope forced participants to manually bridge tokens to the auction’s chain, creating unnecessary delays.</p><p>Without cross-chain capabilities, bidders had to interact with multiple wallets and platforms just to participate, introducing friction at every step. Even simple actions—such as monitoring bids or transferring NFTs—became cumbersome, requiring manual tracking and coordination across different blockchains.Constrained by single-chain limitations and block-level execution, this NFT auction contract fell short of meeting the expectations of the modern multi-chain marketplace.</p><p>What happens when you’re no longer bound by a single block?</p>",
+    "redirect-url":
+      "https://docs.agoric.com/guides/orchestration/contract-walkthroughs/send-anywhere.html",
   },
   {
-    'header-title': 'Here lies an auction contract.',
-    'header-description': 'The bids were high, the competition fierce... until it wasn’t.',
-    'content': "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Outbid by complexity</P>",
-    'main-content': "<p>This auction contract did what it was designed to do—host an auction, gather bids, and transfer assets to the winner when the time expired. However, once the auction ended, so did the contract's purpose. It was limited to a single auction cycle, forced to terminate and redeploy each time a new auction began. For developers, this meant added complexity and overhead. For participants, it meant missed opportunities and the inconvenience of waiting for new deployments.</p><p>This is the fate of short-lived auction contracts: they are built for one-off events. Every new auction requires fresh deployment, creating a fragmented user experience and adding unnecessary friction to what could otherwise be a seamless, continuous marketplace.</p>",
+    "header-title": "Here lies a staking rewards contract",
+    "header-description":
+      "It worked hard, distributed faithfully… but only for a little while.",
+    content:
+      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Limited lifecycle</P>",
+    "main-content":
+      "<p>This staking rewards contract began with the ambition to incentivize participation across chains, ensuring validators and stakers reaped their well-earned rewards. However, it quickly became apparent that it couldn’t keep pace with the multi-chain reality of modern staking. With stakers operating across multiple chains, the complexity multiplied, forcing users to manage rewards and positions manually.</p><p>Calculating interest, updating states, and distributing payouts involved multiple on-chain interactions. Stakers often missed opportunities to rebalance their positions across chains to optimize yields, further limiting their earning potential. Delayed tracking of rewards meant missed chances for re-staking and compounding returns. This staking rewards contract, tied to synchronous execution and atomic limitations, could not endure the demands of a multi-chain ecosystem.</p><p>Release your stake on any chain … programmatically.</p>",
+    "redirect-url":
+      "https://docs.agoric.com/guides/orchestration/contract-walkthroughs/cross-chain-unbond.html",
   },
-
-]
+];
 
 $(document).ready(function () {
+  var soundEffectHover = $("#soundEffectHover")[0];
+  var soundEffectClick = $("#soundEffectClick")[0];
   function adjustImageMap() {
     const viewportWidth = $(window).width();
     const viewportHeight = $(window).height();
@@ -105,7 +131,7 @@ $(document).ready(function () {
 
     const width = maxX - minX;
     const height = maxY - minY;
-    
+
     glowEffect.addClass(areaClass);
 
     // Position and show the glow effect
@@ -120,76 +146,147 @@ $(document).ready(function () {
   }
 
   // Mouseenter event to show the glow effect
-  $("#image-map area").on("mouseenter", function () {
-    const coords = $(this).attr("coords");
-    $(this).css({
-      "cursor": "pointer"
-    });
-    const bgImage = $(this).data("bg-image"); // Get the background image URL from data attribute
-    const areaClass = $(this).data("bg-class"); // Get the background image URL from data attribute
-    positionGlow(coords, bgImage, areaClass);
-  });
+  // $("#image-map area").on("mouseenter", function () {
+  //   const coords = $(this).attr("coords");
+
+  //   $(this).css({
+  //     cursor: "pointer",
+  //   });
+
+  //   soundEffectHover.play();
+
+  //   const bgImage = $(this).data("bg-image"); // Get the background image URL from data attribute
+  //   const areaClass = $(this).data("bg-class"); // Get the background image URL from data attribute
+  //   positionGlow(coords, bgImage, areaClass);
+
+  // });
+
+  $("#image-map area").hover(
+    function () {
+      const coords = $(this).attr("coords");
+
+      $(this).css({
+        cursor: "pointer",
+      });
+      soundEffectHover.currentTime = 0;
+      soundEffectHover.play();
+
+      const bgImage = $(this).data("bg-image"); // Get the background image URL from data attribute
+      const areaClass = $(this).data("bg-class"); // Get the background image URL from data attribute
+      positionGlow(coords, bgImage, areaClass);
+    },
+    function () {
+      // Stop sound when mouse leaves
+      soundEffectHover.pause();
+      soundEffectHover.currentTime = 0; // Reset to start
+    }
+  );
 
   // Mouseleave event to hide the glow effect
   $("#image-map area").on("mouseleave", function () {
     glowEffect.css({
-      "opacity": 0
+      opacity: 0,
     }); // Hide the glow
     const areaClass = $(this).data("bg-class");
     glowEffect.removeClass(areaClass);
+    soundEffectHover.pause();
   });
 
   $("#image-map area").each(function (index) {
-    $(this).click(function() {
+    $(this).click(function () {
+      soundEffectClick.currentTime = 0;
+      soundEffectClick.play();
       const modalId = $(this).data("modal-id");
       $("#areaModal").addClass(modalId);
-      $("#areaModal .modal-body .header .title").text(modalContentArray[index]["header-title"]);
-      $("#areaModal .modal-body .header .description").text(modalContentArray[index]["header-description"]);
-      $("#areaModal .modal-body .content").html(modalContentArray[index]["content"]);
-      $("#areaModal .modal-body .main-content").html(modalContentArray[index]["main-content"]);
+      $("#areaModal .modal-body .header .title").html(
+        modalContentArray[index]["header-title"]
+      );
+      $("#areaModal .modal-body .header .description").text(
+        modalContentArray[index]["header-description"]
+      );
+      $("#areaModal .modal-body .content").html(
+        modalContentArray[index]["content"]
+      );
+      $("#areaModal .modal-body .main-content").html(
+        modalContentArray[index]["main-content"]
+      );
       $("#areaModal").modal("show");
-    })
+    });
   });
 
   // Open the modal automatically on page load
   $("#cemeteryModal").modal("show");
 
   // Music play/stop toggle
-  const musicIcon = document.getElementById("musicIcon");
+  const musicController = $(".music-toggle .music-controller")[0];
+  const soundEffectController = $(".music-toggle .sound-effect-controller")[0];
   const backgroundMusic = document.getElementById("backgroundMusic");
-  let isPlaying = false;
+  let isBgMusicPlaying = false;
+  let isSoundEffectPlaying = false;
 
   // Define paths for play and stop icons
   const playIcon = "./assets/images/voice-icon.svg"; // Replace with your play icon SVG path
   const stopIcon = "./assets/images/voice-off-icon.svg"; // Replace with your stop icon SVG path
 
-  musicIcon.addEventListener("click", () => {
-    if (isPlaying) {
+  musicController.addEventListener("click", () => {
+    if (isBgMusicPlaying) {
       backgroundMusic.pause();
-      musicIcon.src = stopIcon; // Switch to play icon when paused
+      $(this).find(".voiceIcon1").attr("src", stopIcon); // Switch to play icon when paused
     } else {
       backgroundMusic.play();
-      musicIcon.src = playIcon; // Switch to stop icon when playing
+      $(this).find(".voiceIcon1").attr("src", playIcon); // Switch to stop icon when playing
     }
-    isPlaying = !isPlaying;
+    isBgMusicPlaying = !isBgMusicPlaying;
+  });
+
+  soundEffectController.addEventListener("click", () => {
+    if (isSoundEffectPlaying) {
+      soundEffectHover.muted = false;
+      soundEffectClick.muted = false;
+      $(this).find(".voiceIcon2").attr("src", playIcon); // Switch to play icon when paused
+    } else {
+      soundEffectHover.muted = true;
+      soundEffectClick.muted = true;
+      $(this).find(".voiceIcon2").attr("src", stopIcon); // Switch to stop icon when playing
+    }
+    isSoundEffectPlaying = !isSoundEffectPlaying;
   });
 
   $(".custom-modal .enter-button").click(function () {
     backgroundMusic.play();
-    isPlaying = true;
+    isBgMusicPlaying = true;
     $(".custom-modal").modal("hide");
   });
 
   $("#areaModal .enter-button").click(function () {
     $("#areaModal").modal("hide");
-    var classListStr = $("#areaModal").attr('class');
-    if (classListStr.search('modal-area') !== -1) {
-      var modalNumClass = classListStr.substr(classListStr.search('modal-area'), 11);
+    var classListStr = $("#areaModal").attr("class");
+    if (classListStr.search("modal-area") !== -1) {
+      var modalNumClass = classListStr.substr(
+        classListStr.search("modal-area"),
+        11
+      );
+      var lastStr = modalNumClass.charAt(10);
+      $(this).attr("href", modalContentArray[lastStr]["redirect-url"]);
       $("#areaModal").css({
-        display: "none"
-      })
+        display: "none",
+      });
       $("#areaModal").removeClass(modalNumClass);
     }
   });
-
+  $("#areaModal").on("hidden.bs.modal", function () {
+    // put your default event here
+    $("#areaModal").modal("hide");
+    var classListStr = $("#areaModal").attr("class");
+    if (classListStr.search("modal-area") !== -1) {
+      var modalNumClass = classListStr.substr(
+        classListStr.search("modal-area"),
+        11
+      );
+      $("#areaModal").css({
+        display: "none",
+      });
+      $("#areaModal").removeClass(modalNumClass);
+    }
+  });
 });
