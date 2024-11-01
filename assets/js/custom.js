39aac07584f532f11a0dbf30e9a826a35bae3f6f
leadcyber <travashley826@gmail.com>
2024-11-01 13:02:40 -0500

    fixed modal design and font issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@ const modalContentArray = [
     "header-description":
       "Loans were given, collateral secured... until cross-chain complexity stopped it in its tracks",
     content:
-      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Bound by atomic operations</P>",
+      "<P><span class='content-span'>Deployed</span> Block 4815162342</P><P><span class='content-span'>Expired</span> Block 4815162343</P><P><span class='content-span'>Cause of death</span> Bound by atomic operations</P>",
     "main-content":
       "<p>This lending platform faithfully executed its duties, offering loans and securing collateral on-chain with precision. Liquidations were fast, and interest rates competitive—for as long as everything happened within a single ecosystem. However, as users sought multi-chain lending capabilities, cracks began to show. It struggled to keep pace in a world where users wanted to borrow from one chain while collateral resided on another.</p><p>If only it had been built for asynchronous execution! Asynchronous liquidation triggers could monitor loans across chains, initiating liquidations even if collateral and debt resided on separate platforms. Liquidity aggregation could have drawn from multiple chains in real-time, optimizing interest rates and lowering loan-to-value risks without interruptions.</p><p>Orchestration would have brought this platform longer life.</p>",
     "redirect-url":
@@ -18,7 +18,7 @@ const modalContentArray = [
     "header-description":
       "The returns were optimized, the profits were promising... but only within one chain",
     content:
-      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Single-chain constriction</P>",
+      "<P><span class='content-span'>Deployed</span> Block 4815162342</P><P><span class='content-span'>Expired</span> Block 4815162343</P><P><span class='content-span'>Cause of death</span> Single-chain constriction</P>",
     "main-content":
       "<p>This yield aggregator set out to provide users with the best returns by shifting funds between high-yield opportunities—as long as everything remained on a single chain. It worked well within its own ecosystem, but the moment users sought higher yields across multiple blockchains, the aggregator faltered. It couldn’t manage rebalancing strategies across networks.</p><p>It could have asynchronously rebalanced vaults between multiple chains. Instead of relying on instant atomic operations, the aggregator could have shifted to an adaptive, asynchronous strategy that maximized returns smoothly across ecosystems. Unfortunately, limited to a single chain and real-time constraints, this yield aggregator missed its chance to thrive in a multi-chain world. </p> <p>With asynchronous contracts, this platform’s ending may have been different.</p>",
     "redirect-url":
@@ -29,7 +29,7 @@ const modalContentArray = [
     "header-description":
       "The fees were steady, the renewals consistent... until they weren’t.",
     content:
-      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Buried under user intents</P>",
+      "<P><span class='content-span'>Deployed</span> Block 4815162342</P><P><span class='content-span'>Expired</span> Block 4815162343</P><P><span class='content-span'>Cause of death</span> Buried under user intents</P>",
     "main-content":
       "<p>This subscription contract began with the goal of providing automated, recurring payments to ensure uninterrupted services for users. It operated well initially—until the complexities of modern blockchain environments overwhelmed it. Bound to a single chain, the contract couldn’t keep up as users demanded multi-chain payment options.</p><p>Every payment required the user’s active approval, which introduced friction, and lapses became common as users failed to keep up with approvals. As the contract tried to manage every interaction atomically, it was buried by the weight of complexity and manual intervention. Alas, this contract was constrained by outdated execution models, unable to meet the demands of a multi-chain world.</p><p>Orchestration accounts are an async secret weapon.</p>",
     "redirect-url":
@@ -40,7 +40,7 @@ const modalContentArray = [
     "header-description":
       "The bids were high, the competition fierce... until it wasn’t.",
     content:
-      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Block-constrained execution</P>",
+      "<P><span class='content-span'>Deployed</span> Block 4815162342</P><P><span class='content-span'>Expired</span> Block 4815162343</P><P><span class='content-span'>Cause of death</span> Block-constrained execution</P>",
     "main-content":
       "<p>This NFT auction contract began with the promise of decentralized ownership and open competition. Yet, the excitement quickly waned as participants encountered the limitations of their single-chain environment. When people routinely have NFTs and liquidity spanning multiple chains, this contract’s limited scope forced participants to manually bridge tokens to the auction’s chain, creating unnecessary delays.</p><p>Without cross-chain capabilities, bidders had to interact with multiple wallets and platforms just to participate, introducing friction at every step. Even simple actions—such as monitoring bids or transferring NFTs—became cumbersome, requiring manual tracking and coordination across different blockchains.Constrained by single-chain limitations and block-level execution, this NFT auction contract fell short of meeting the expectations of the modern multi-chain marketplace.</p><p>What happens when you’re no longer bound by a single block?</p>",
     "redirect-url":
@@ -51,7 +51,7 @@ const modalContentArray = [
     "header-description":
       "It worked hard, distributed faithfully… but only for a little while.",
     content:
-      "<P>Deployed: Block 4815162342</P><P>Expired: Block 4815162343</P><P>Cause of death: Limited lifecycle</P>",
+      "<P><span class='content-span'>Deployed</span> Block 4815162342</P><P><span class='content-span'>Expired</span> Block 4815162343</P><P><span class='content-span'>Cause of death</span> Limited lifecycle</P>",
     "main-content":
       "<p>This staking rewards contract began with the ambition to incentivize participation across chains, ensuring validators and stakers reaped their well-earned rewards. However, it quickly became apparent that it couldn’t keep pace with the multi-chain reality of modern staking. With stakers operating across multiple chains, the complexity multiplied, forcing users to manage rewards and positions manually.</p><p>Calculating interest, updating states, and distributing payouts involved multiple on-chain interactions. Stakers often missed opportunities to rebalance their positions across chains to optimize yields, further limiting their earning potential. Delayed tracking of rewards meant missed chances for re-staking and compounding returns. This staking rewards contract, tied to synchronous execution and atomic limitations, could not endure the demands of a multi-chain ecosystem.</p><p>Release your stake on any chain … programmatically.</p>",
     "redirect-url":
